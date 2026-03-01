b880edfa011d9ca3e88aa715ff25b863d5db9bf5
nyaito-mine <nyaito0414@gmail.com>
2026-03-01 15:40:01 +0900

    Fix

--- src/main/kotlin/co/skyblock/features/nyahirunaddons/na1/disableUse/sbMenu.kt
+++ src/main/kotlin/co/skyblock/features/nyahirunaddons/na1/disableUse/sbMenu.kt
@@ -2,6 +2,7 @@ package co.skyblock.features.nyahirunaddons.na1.disableUse
 
 import co.skyblock.events.core.InteractionEvent
 import co.stellarskys.stella.annotations.Module
+import co.stellarskys.stella.events.core.TickEvent
 import co.stellarskys.stella.features.Feature
 import co.stellarskys.stella.utils.config
 import net.minecraft.world.InteractionResult
@@ -15,13 +16,25 @@ object sbMenu : Feature("disableUse", island = SkyBlockIsland.THE_CATACOMBS) {
 
     val sbMenuConfig by config.property<Boolean>("disableUseSBMenu")
 
+    private var relicTickTime = 0
+
     override fun initialize() {
+        on<TickEvent.Client> {
+            if (relicTickTime > 0) relicTickTime--
+        }
+
         on<InteractionEvent.ItemUseAttempt> { event ->
             if (!sbMenuConfig) return@on
             val stack = event.stack
             val useItem = stack.item
+            val isRelic = useItem.name.string.contains("Corrupted") && useItem.name.string.contains("Relic")
 
             if (useItem == NETHER_STAR || useItem == FILLED_MAP || useItem == FEATHER) {
+                event.result = InteractionResult.FAIL
+                event.cancel()
+            }
+
+            if (isRelic && relicTickTime > 0) {
                 event.result = InteractionResult.FAIL
                 event.cancel()
             }
@@ -33,10 +46,18 @@ object sbMenu : Feature("disableUse", island = SkyBlockIsland.THE_CATACOMBS) {
             if (!sbMenuConfig) return@on
             val stack = event.stack
             val useItem = stack.item
+            val isRelic = useItem.name.string.contains("Corrupted") && useItem.name.string.contains("Relic")
 
             if (useItem == NETHER_STAR || useItem == FILLED_MAP || useItem == FEATHER) {
                 event.result = InteractionResult.FAIL
                 event.cancel()
+            }
+
+            if (isRelic && relicTickTime > 0) {
+                event.result = InteractionResult.FAIL
+                event.cancel()
+            } else if (isRelic && relicTickTime == 0) {
+                relicTickTime = 5
             }
 
             return@on
@@ -46,8 +67,14 @@ object sbMenu : Feature("disableUse", island = SkyBlockIsland.THE_CATACOMBS) {
             if (!sbMenuConfig) return@on
             val stack = event.stack
             val useItem = stack.item
+            val isRelic = useItem.name.string.contains("Corrupted") && useItem.name.string.contains("Relic")
 
             if (useItem == NETHER_STAR || useItem == FILLED_MAP || useItem == FEATHER) {
+                event.result = true
+                event.cancel()
+            }
+
+            if (isRelic && relicTickTime > 0) {
                 event.result = true
                 event.cancel()
             }
