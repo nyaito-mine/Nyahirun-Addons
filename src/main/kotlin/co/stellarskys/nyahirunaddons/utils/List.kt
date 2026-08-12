dec283f5c8d2e9fffadf6bf0980e2dfcc31158a1
nyaito-mine <nyaito0414@gmail.com>
2026-08-13 02:19:32 +0900

    1.0.6a3

--- src/main/kotlin/co/stellarskys/nyahirunaddons/utils/List.kt
+++ src/main/kotlin/co/stellarskys/nyahirunaddons/utils/List.kt
@@ -1,30 +1,5 @@
 package co.stellarskys.nyahirunaddons.utils
 
-data class ListEntry(
-    val enabled: () -> Boolean,
-    val stringFirst: String,
-    val stringSecond: String = "",
-    val stringThird: String = "",
-    val stringFourth: String = "",
-    val stringFifth: String = "",
-) {
-    val triggers: List<String> by lazy(LazyThreadSafetyMode.NONE) {
-        listOf(stringFirst, stringSecond, stringThird, stringFourth, stringFifth)
-            .filter(String::isNotBlank)
-    }
-}
-
-data class ListEntryStringInt(
-    val enabled: () -> Boolean,
-    val string: String,
-    val int: Int = 0,
-)
-
-data class ListEntryStringColorInt(
-    val string: String,
-    val color: Int = 0xFF404040.toInt()
-)
-
 data class Quadruple(
     val first: Any,
     val second: Any,
